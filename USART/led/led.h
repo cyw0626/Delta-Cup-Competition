a4edfa646a1b6f7cba97ed907d7f5bdab5475108
Yuwang Cui <44495079+cyw0626@users.noreply.github.com>
2019-07-18 09:51:54 +0800

    Update led.h

--- USART/led/led.h
+++ USART/led/led.h
@@ -5,9 +5,9 @@
 #define ON  0
 #define OFF 1
 #define LED1(a) if(a) \
-											GPIO_SetBits(GPIOB,GPIO_Pin_14); \
-								else \
-											GPIO_ResetBits(GPIOB,GPIO_Pin_14)
+		      GPIO_SetBits(GPIOB,GPIO_Pin_14); \
+		else \
+		     GPIO_ResetBits(GPIOB,GPIO_Pin_14)
 
 void LED_Init(void);    //初始化
 void Led_Test(void);
